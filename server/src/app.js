86d71a7b49c990c0602b9d284e39fa8d7a778d3d
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-10-05 18:52:51 +0100

    Added example route

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+const PORT = 4000;
+
+// define routes
+app.use('/example', require('./routes/example.js'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
