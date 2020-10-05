8335999f3ceeb279761b30a042b9e0bda35f5b91
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-10-05 17:24:30 +0100

    added example test

---
+++ server/test/example.test.js
@@ -0,0 +1,13 @@
+const { assert } = require('chai');
+
+// chai assertion library docs : https://www.chaijs.com/api/assert/
+
+describe('filename', () => {
+  describe('functionName', () => {
+    it('should do this', () => {
+      // test
+      // assert
+      assert.isTrue(true);
+    });
+  });
+});
