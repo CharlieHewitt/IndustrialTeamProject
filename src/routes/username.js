e2692ffc6711e000afcdbec06944561dd1e6d125
John <johngrossi1999@hotmail.com>
2020-10-08 10:54:40 +0100

    receives username and returns it with an ID and score of zero, with amendments

--- src/routes/username.js
+++ src/routes/username.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/player')
+const User = require('../storage/newUser')
 
 // @route   POST /api/username/send
 // @desc    Recieve username and give it an ID and Score
 router.post('/send', (req, res) => {
   const username = req.body.username;
 
+  //user doesn't put anything as they're name
   if (username == null) {
       //Kamila's method call
   }
@@ -15,15 +16,11 @@ router.post('/send', (req, res) => {
   id = createID();
   score = 0;
 
-  userStats = new User({
-      username,
-      id,
-      score
-  })
-
+  userStats = new User({username, id, score})
   res.json(userStats)
 });
 
+//create a random ID for new user
 function createID(){
     var seq = Math.floor(1000 + Math.random() * 9000)
     return seq;
