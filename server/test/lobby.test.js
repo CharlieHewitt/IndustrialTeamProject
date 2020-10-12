c06965d2ac9e4e4140da25f9693e9b6a3e907b4e
John <johngrossi1999@hotmail.com>
2020-10-12 18:47:44 +0100

    created tests for lobby class, fixed format

--- server/test/lobby.test.js
+++ server/test/lobby.test.js
@@ -5,29 +5,33 @@ const User = require('../src/storage/newUser');
 
 // chai assertion library docs : https://www.chaijs.com/api/assert/
 
-describe('lobby', () => {
-  describe('createLobbyID', () => {
-    let lobby = new Lobby(new User("user1", "1234", 0));
-    let result = lobby.createLobbyID();
-    it ('should return a non-empty variable', () => {
-      assert.isNotNull(result);
-    });
-    it ('should return a string', () => {
-      assert.isString(result);
-    });
-  });
-
-  describe('addPlayer', () => {
+describe('lobby class', () => {
     const firstPlayer = new User("user1", "1234", 0);
     const secondPlayer = new User("user2", "4321", 0);
     const lobby = new Lobby(firstPlayer);
     
-    lobby.addPlayer(secondPlayer);
-    it ('should have added a player to lobby', () => {
-        assert.equal(lobby.players[firstPlayer.id], firstPlayer);
+    describe('initialising lobby', () => {
+        it ('adds host to the lobby', () => {
+            assert.equal(lobby.players[firstPlayer.id], firstPlayer);
+        });
     });
-    it ('should have added correct player to lobby', () => {
-        assert.equal(lobby.players[secondPlayer.id], secondPlayer);
+
+    describe('createLobbyID function', () => {
+        let lobby = new Lobby(firstPlayer);
+        let result = lobby.createLobbyID();
+        it ('should return a non-empty variable', () => {
+        assert.isNotNull(result);
+        });
+        it ('should return a string', () => {
+        assert.isString(result);
+        });
     });
-  });
+
+    describe('addPlayer function', () => {
+        lobby.addPlayer(secondPlayer);
+        it ('should have added correct player to lobby', () => {
+            assert.equal(lobby.players[secondPlayer.id], secondPlayer);
+        });
+        //TODO check for duplicates
+    });
 });
