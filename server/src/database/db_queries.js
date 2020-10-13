484591143b343915baa72e102678586d5dcc8997
neildesa <neilsondesa@gmail.com>
2020-10-13 13:43:16 +0100

    Added starter code for db query files

---
+++ server/src/database/db_queries.js
@@ -0,0 +1,6 @@
+const MultiChoiceQuestionModel = require('../models/multiChoiceQuestionSchema');
+const BooleanQuestionModel = require('../models/booleanQuestionSchema');
+
+function getMultiChoiceQuestion(category, numOfQuestions) {
+  const multiChoiceQuestion = new MultiChoiceQuestionModel();
+}
