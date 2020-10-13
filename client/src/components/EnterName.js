99910a81edf79c40c8b09f521211954f982e25ca
gmitchell2000 <gmitchell2000@gmail.com>
2020-10-13 11:29:08 +0100

    adding of the homepage

---
+++ client/src/components/EnterName.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class EnterName extends Component {
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text" 
+                    name="title" 
+                    style={{
+                        backgroundColor: '#ffffff',
+                        width: '890px', 
+                        height: '130px',
+                        position: 'absolute',
+                        fontSize: '70px',
+                        left: '515px',
+                        top:'443px'
+                    }}
+                    placeholder="Enter name..." 
+                />
+            </form>
+        )
+    }
+}
+
+export default EnterName
