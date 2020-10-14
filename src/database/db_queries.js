83174942e8326cd6dc208022b0bcb6d34bbefa79
neildesa <neilsondesa@gmail.com>
2020-10-14 13:43:06 +0100

    Changed db queries to async

--- src/database/db_queries.js
+++ src/database/db_queries.js
@@ -2,7 +2,8 @@ const MultiChoiceQuestionModel = require('../models/multiChoiceQuestionSchema');
 const BooleanQuestionModel = require('../models/booleanQuestionSchema');
 
 /**
- * Returns a list of json documents that belong to the requested category.
+ * Returns a list of json documents that correspond to all
+ * multi-choice questions that belong to the requested category.
  *
  * @param {string} requestedCategory - category name
  */
@@ -14,7 +15,7 @@ function getAllMultiChoiceQuestions(requestedCategory) {
     if (err) return handleError(err);
     if (document.length === 0) {
       console.log(
-        'document length is 0 - category may not exist; please check category is spelt correctly.'
+        `document length is 0 - ${requestedCategory} may not exist; please check category is spelt correctly.`
       );
     } else {
       console.log(document);
@@ -22,6 +23,46 @@ function getAllMultiChoiceQuestions(requestedCategory) {
   });
 }
 
-function getRandomMultiChoiceQuestions(requestedCategory, numOfQuestions) {}
+/**
+ * Gets an array of json documents that correspond to a random
+ * selection of multi-choice questions from the requested category.
+ *
+ * @param {string} requestedCategory - category name
+ * @param {number} numOfQuestions - number of questions requested
+ * @returns array of multi-choice question objects
+ */
+async function getRandomMultiChoiceQuestions(
+  requestedCategory,
+  numOfQuestions
+) {
+  let count = 0;
+  const randomMultiChoiceQuestions = [];
 
-module.exports = getAllMultiChoiceQuestions;
+  for (count; count < numOfQuestions; count++) {
+    const random = Math.floor(Math.random() * 42);
+    await getMCQuestion(requestedCategory, random)
+      .then((dbResponse) => {
+        if (randomMultiChoiceQuestions.includes(dbResponse)) {
+          count--;
+        } else {
+          randomMultiChoiceQuestions.push(dbResponse);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return randomMultiChoiceQuestions;
+}
+
+async function getMCQuestion(requestedCategory, random) {
+  return MultiChoiceQuestionModel.findOne({ category: requestedCategory })
+    .skip(random)
+    .exec();
+}
+
+module.exports = {
+  getAllMultiChoiceQuestions: getAllMultiChoiceQuestions,
+  getRandomMultiChoiceQuestions: getRandomMultiChoiceQuestions,
+};
