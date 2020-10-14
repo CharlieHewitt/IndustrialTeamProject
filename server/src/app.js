9ef8702ef246003fe7ae4d3692908ca912e3d219
John <johngrossi1999@hotmail.com>
2020-10-14 16:30:59 +0100

    show players in lobby w/route

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const connectToDatabase = require("./database/db_connection.js");
+const Lobbies = require('./storage/lobbyManager');
+const Lobby = require('./storage/lobby');
+const User = require('./storage/newUser');
 
 // import env variables
 // Create .env file if you haven't!
@@ -11,14 +14,15 @@ const MONGODB_CONNECTIONSTRING = process.env.MONGODB_CONNECTIONSTRING;
 const app = express();
 
 app.use(express.json());
+app.locals.allLobbies = new Lobbies();
 
 connectToDatabase(MONGODB_CONNECTIONSTRING);
 
 // define routes
 app.use("/example", require("./routes/example.js"));
 app.use("/api/username", require("./routes/username.js"));
+app.use("/api/lobby", require("./routes/lobby.js"));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-
---
+++ server/src/routes/lobby.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const Lobbbies = require('../storage/lobbyManager');
+const Lobby = require('../storage/lobby');
+const User = require('../storage/newUser');
+
+// @route   GET /api/lobby/getLobbyPlayers
+// @desc    return everyone in the lobby
+router.get('/getLobbyPlayers', (req, res) => {
+    var lobbies = req.app.locals.allLobbies;
+    var wantedID = req.body.id;
+  
+    var wantedLobby = lobbies.getLobby(wantedID);
+
+    res.json(wantedLobby.players);
+});
+
+module.exports = router;
