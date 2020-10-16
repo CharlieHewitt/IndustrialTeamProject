911ffb76846e078a1d2f08b0bb9002558369679d
PeterShort <petershort1507@gmail.com>
2020-10-16 11:18:30 +0100

    Folder Name Change
    Just changed the name of the "Modal" folder to "SettingsMenu" in order for it to not conflict with an existing folder with the same name.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./Routes";
-import Settings from './components/Modal/Settings';
+import Settings from './components/SettingsMenu/Settings';
 
 ReactDOM.render(
   <React.StrictMode>
