4c7d2c79b5493fc0cc8b5c89ba897be441e8dcc2
Kamila Gorska <kgorska@dundee.ac.uk>
2020-10-16 10:37:38 +0100

    lobby routes added

--- src/routes/username.js
+++ src/routes/username.js
@@ -3,19 +3,46 @@ const router = express.Router();
 
 const User = require('../storage/newUser');
 
-// @route   POST /api/username/send
+// @route   POST /api/lobby/join
 // @desc    Recieve username and give it an ID and Score
 router.post('/send', (req, res) => {
-  const username = req.body.username;
+  const username = req.body.playerName;
 
   id = createID();
   score = 0;
 
   userStats = new User(username, id, score);
-  lobbyId = req.body.lobbyID;
+  lobbyId = req.body.lobbyId;
   lobbyManager = req.app.locals.allLobbies;
   var success = lobbyManager.joinLobby(lobbyId, userStats);
-  res.json(success) 
+
+  const responseObject = {};
+  responseObject["success"] = success;
+  responseObject["lobbyId"] = lobbyId;
+  responseObject["playerId"] = userStats.id;
+  responseObject["playerName"] = userStats.username;
+  res.json(responseObject);
+
+});
+
+// @route   POST /api/lobby/create
+// @desc    create lobby and add host
+router.post('/createLobby', (req, res) => {
+    var lobbies = req.app.locals.allLobbies;
+    var hostUsername = req.body.hostName;
+    var categories = req.body.categories;
+    var id = createID();
+
+    var user = new User(hostUsername, id, 0);
+    var lobby = new Lobby(categories, user);
+
+    var lobbyid = lobby.lobbyID;
+
+    const responseObject = {};
+    responseObject["lobbyId"] = lobbyid;
+    responseObject["hostId"] = id;
+    responseObject["hostName"] = user.username;
+    res.json(responseObject);
 });
 
 //create a random ID for new user
