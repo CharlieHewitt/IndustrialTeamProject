059effdc2cdeea0cf9a5fbdc16e071ed1049d152
Justas Samuolis <samuolis.just@gmail.com>
2020-10-16 11:55:10 +0100

    Remove duplicates

--- client/src/index.js
+++ client/src/index.js
@@ -19,25 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.less";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Routes } from "./Routes";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <div className="app">
-      <Router>
-        <Routes />
-      </Router>
-    </div>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
