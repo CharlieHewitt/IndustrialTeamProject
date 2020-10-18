bd2c523c8f5d8cb9d3292d72181d8646e7c3e998
Kamila Gorska <kgorska@dundee.ac.uk>
2020-10-18 19:20:19 +0100

    added tests for lobbyManager class

--- server/test/lobbyManager.test.js
+++ server/test/lobbyManager.test.js
@@ -4,17 +4,37 @@ const LobbyManager = require('../src/storage/lobbyManager');
 const User = require('../src/storage/newUser');
 
 describe('lobbyManager class', () => {
+  var lobbyManager = new LobbyManager();
+  var host = new User("host", "111", 0);
+  const categs = ["general knowledge", "abc"];
+  var lobby = new Lobby(host, categs);
+
+  describe('addLobby method', () => {
+    it('should add lobby to lobbyManager', () => {
+      lobbyManager.addLobby(lobby);
+      assert.containsAllKeys(lobbyManager.lobbies, lobby.lobbyID);
+    });
+  });
+  describe('checkLobbyValid method', () => {
+    it('should check if lobby exists', () => {
+      var result = lobbyManager.checkLobbyValid(lobby.lobbyID);
+      assert.equal(result, true);
+    });
+  });
   describe('joinLobby method', () => {
     it('should add player to lobby', () => {
-      var lobbyManager = new LobbyManager();
-      var host = new User("host", "111", 0);
-      var lobby = new Lobby(host);
       var lobId = lobby.lobbyID;
       lobbyManager.addLobby(lobby);
       var player = new User("player1", "112", 0);
       lobbyManager.joinLobby(lobId, player);
-
       assert.containsAllKeys(lobby.players, player.id);
     });
   });
+  describe('getLobby method', () => {
+    it('should return lobby when given lobby id', () => {
+      result = lobbyManager.getLobby(lobby.lobbyID);
+      assert.equal(result, lobby);
+    });
+  });
+
 });
