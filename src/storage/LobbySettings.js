654cc0833bbf7b66477d041be9bb6cbf918c24bc
neildesa <neilsondesa@gmail.com>
2020-10-19 12:10:55 +0100

    added skeleton structure

--- src/storage/LobbySettings.js
+++ src/storage/LobbySettings.js
@@ -3,86 +3,84 @@ const DEFAULT_ANSWER_TIME = 10; // in seconds
 const DEFAULT_NUM_QUESTIONS = 20;
 
 class LobbySettings {
-    constructor() {
-        this.answerTime = DEFAULT_ANSWER_TIME;
-        this.numQuestions = DEFAULT_NUM_QUESTIONS;
-        this.categories = []; 
+  constructor() {
+    this.answerTime = DEFAULT_ANSWER_TIME;
+    this.numQuestions = DEFAULT_NUM_QUESTIONS;
+    this.categories = [];
+  }
+
+  updateAnswerTime(answerTime) {
+    if (typeof answerTime != 'number') {
+      console.error('Error: argument was NaN');
+      return false;
     }
 
-    updateAnswerTime(answerTime) {
-        if (typeof answerTime != 'number')
-        {
-            console.error('Error: argument was NaN');
-            return false;
-        }
+    if (answerTime <= 0) {
+      console.error(
+        'Error: time to answer a question cant be less than or equal to 0 seconds.'
+      );
+      return false;
+    }
+    this.answerTime = answerTime;
 
-        if (answerTime <= 0)
-        {
-            console.error('Error: time to answer a question cant be less than or equal to 0 seconds.');
-            return false;
-        }
-        this.answerTime = answerTime;
-        
-        return true;
+    return true;
+  }
+
+  updateNumQuestions(numQuestions) {
+    if (typeof numQuestions != 'number') {
+      console.error('Error: argument was NaN');
+      return false;
     }
 
-    updateNumQuestions(numQuestions) {
-        if (typeof numQuestions != 'number')
-        {
-            console.error('Error: argument was NaN');
-            return false;
-        }
-
-        if (numQuestions <= 0)
-        {
-            console.error("Error: number of questions can't be less than or equal to 0");
-            return false;
-        }
-
-        this.numQuestions = numQuestions;
-        return true;
+    if (numQuestions <= 0) {
+      console.error(
+        "Error: number of questions can't be less than or equal to 0"
+      );
+      return false;
     }
 
-    updateCategories(categories){
-        if (!Array.isArray(categories))
-        {
-            console.error("categories was not an array");
-            return false;
-        }
-        // error handling on valid categories? shouldn't be needed if frontend is using categories from the API (display purposes only from here onwards)
-        if(categories.length === 0)
-        {
-            console.error("No categories given");
-            return false;
-        }
+    this.numQuestions = numQuestions;
+    return true;
+  }
 
-        this.categories = categories;
-        return true;
+  updateCategories(categories) {
+    if (!Array.isArray(categories)) {
+      console.error('categories was not an array');
+      return false;
+    }
+    // error handling on valid categories? shouldn't be needed if frontend is using categories from the API (display purposes only from here onwards)
+    if (categories.length === 0) {
+      console.error('No categories given');
+      return false;
     }
 
-    // may add below function in the future
+    this.categories = categories;
+    return true;
+  }
 
-    // takes in a settings object containing the following:
-    // updateSettings({numQuestions, answerTime, categories}){
-    //     let success = true;
-    //     success = this.updateNumQuestions(numQuestions);
+  // may add below function in the future
 
-    //     // Abort on error
-    //     if (!success){
-    //         return false;
-    //     }
+  // takes in a settings object containing the following:
+  // updateSettings({numQuestions, answerTime, categories}){
+  //     let success = true;
+  //     success = this.updateNumQuestions(numQuestions);
 
-    //     success = this.updateAnswerTime(answerTime);
+  //     // Abort on error
+  //     if (!success){
+  //         return false;
+  //     }
 
-    //     // Abort on error
-    //     if (!success){
-    //         return false;
-    //     }
+  //     success = this.updateAnswerTime(answerTime);
 
-    //     success = this.updateCategories(categories);
+  //     // Abort on error
+  //     if (!success){
+  //         return false;
+  //     }
 
-    //     return success;
-    // }
+  //     success = this.updateCategories(categories);
+
+  //     return success;
+  // }
 }
 
 module.exports = LobbySettings;
