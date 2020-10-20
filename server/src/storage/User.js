779ad81ce90b01fe30ba3637a4613ba11dd22e45
John <johngrossi1999@hotmail.com>
2020-10-20 20:10:18 +0100

    50/50 hint

---
+++ server/src/storage/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(username, id, score) {
+    //may add more things to this in the future
+    this.username = username;
+    this.id = id;
+    this.score = score;
+    this.fiftyFifty = false;
+  }
+}
+
+module.exports = User;
