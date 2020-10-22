f13742aa8aadada2b69ed56d2fb262990b0529e6
affyzal <32396442+affyzal@users.noreply.github.com>
2020-10-22 13:08:21 +0100

    made all routes async

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -7,7 +7,7 @@ const User = require('../storage/user');
 
 // @route   POST /api/lobby/join
 // @desc    A player can 'join' a lobby by using this link
-router.post('/join', (req, res) => {
+router.post('/join', async (req, res) => {
   const username = req.body.playerName;
 
   id = createID();
@@ -31,7 +31,7 @@ router.post('/join', (req, res) => {
 
 // @route   POST /api/lobby/create
 // @desc    create lobby and add host
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   var lobbies = req.app.locals.allLobbies;
   var hostUsername = req.body.hostName;
   var id = createID();
@@ -52,7 +52,7 @@ router.post('/create', (req, res) => {
 
 // @route   POST /api/lobby/getLobbyPlayers
 // @desc    return everyone in the lobby
-router.post('/getLobbyPlayers', (req, res) => {
+router.post('/getLobbyPlayers', async (req, res) => {
   var lobbies = req.app.locals.allLobbies;
   const { lobbyId } = req.body;
 
