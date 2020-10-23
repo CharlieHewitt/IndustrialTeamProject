fd734594755bc50592e04a78b51533ea47ad3631
Justas Samuolis <samuolis.just@gmail.com>
2020-10-23 14:51:23 +0300

    fix number suffix function

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,5 @@
 export function getNumberSuffix(number) {
+  if (number % 100 > 10 && number % 100 < 20) return "TH";
   if (number % 10 <= 1) return "ST";
   if (number % 10 == 2) return "ND";
   if (number % 10 == 3) return "RD";
