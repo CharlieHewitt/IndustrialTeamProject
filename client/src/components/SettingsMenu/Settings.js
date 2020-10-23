76ee335bb599343c18ca86e951615bd12f31428f
PeterShort <petershort1507@gmail.com>
2020-10-23 13:07:54 +0100

    Options Menu
    Removed the volume slider, changed the size of the settings menu.
    Changed colour of settings overlay.
    Placed buttons in modal.
    Deleted index.css in settingsmenu folder as no longer needed for slider.

--- client/src/components/SettingsMenu/Settings.js
+++ client/src/components/SettingsMenu/Settings.js
@@ -6,22 +6,19 @@ const BUTTON_WRAPPER_STYLES = {
   position: 'fixed',
   top: 5,
   right: 5,
-  zIndex: 1
+  zIndex: 1,
 }
 
-export default function Settings() {
+export default function Options() {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <>
-      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('Settings Opened')}>
+      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('Options Opened')}>
         <button onClick={() => setIsOpen(true)}>Settings</button>
 
         <Modal open={isOpen} onClose={() => setIsOpen(false)}>
           <body>
-              <h1>Settings Menu</h1>
-            <p>Volume</p>
-            <p>Fullscreen</p>
-            <p>Return to game</p>
+              <h1>Options</h1>
           </body>
         </Modal>
       </div>
